55e5ad8b28658dc6ed4db43a9917d30543a49443
Frariroc <shivakhitaulya@gmail.com>
2026-09-14 11:58:01 +0530

    Fix component file names

--- server/server.js
+++ server/server.js
@@ -86,6 +86,8 @@ ${message}
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });

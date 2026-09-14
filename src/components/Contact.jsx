0b4c4b580ddef50e5c3d08f8d9f2f516d3395c7b
Frariroc <shivakhitaulya@gmail.com>
2026-09-14 12:16:03 +0530

    Fix component file names

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,14 +29,16 @@ const Contact = () => {
     setStatus("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(form),
-      });
-
+     const response = await fetch(
+  "https://shiva-portfolio-atml.onrender.com/api/contact",
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(form),
+  }
+);
       const data = await response.json();
 
       if (!response.ok) {
